22f6c3d22391f68d81cdb22b6ace8cbb0571351e
androitrix <androitrix@gmail.com>
2015-07-08 18:48:13 +1200

    2nd

--- src/main/java/org/ndas/deliverit/controller/LoginController.java
+++ src/main/java/org/ndas/deliverit/controller/LoginController.java
@@ -1,7 +1,7 @@
 package org.ndas.deliverit.controller;
 
 import org.ndas.deliverit.form.LoginForm;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.ndas.deliverit.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
-
-	@Autowired
-	private UserService userService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String login() {
@@ -22,7 +19,9 @@ public class LoginController {
 	@RequestMapping(value="login", method=RequestMethod.POST)
 	public ModelAndView loginProcess(@ModelAttribute LoginForm loginForm, ModelAndView model) {
 		model.setViewName("home");
-		model.addObject("user", new User(loginForm.getEmailAddress()));
+		User user = new User(loginForm.getEmailAddress());
+		user.setFirstName("Sharuyr");
+		model.addObject("user", user);
 		return model;
 	}
 }
