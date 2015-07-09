d663a7836f06aa70c6b3dded810462622ed9e2a6
androitrix <androitrix@gmail.com>
2015-07-10 09:09:51 +1200

    removed target files

--- src/main/java/org/ndas/deliverit/web/controller/JobController.java
+++ src/main/java/org/ndas/deliverit/web/controller/JobController.java
@@ -3,13 +3,14 @@ package org.ndas.deliverit.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JobController {
 	
 	@RequestMapping(value ="jobs", method=RequestMethod.GET)
 	public ModelAndView jobs() {
-		
+		return null;
 	}
  
 }
