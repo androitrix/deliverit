2355db6785110489f22e518e23c77cff0a7b89ae
androitrix <androitrix@gmail.com>
2015-07-10 16:08:08 +1200

    jobcreate html

--- src/main/java/org/ndas/deliverit/data/Job.java
+++ src/main/java/org/ndas/deliverit/data/Job.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="job")
 public class Job {
 
 	@Id
-	@Column(name = "job")
+	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
